233e38671b37b96d3ee9aa18b1412a6bd6034f4e
Daniel Furman <djfurman@gmail.com>
2018-08-19 17:11:05 -0400

    Add aws-xray-sdk and conditional usage

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,10 @@
-from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.core import patch_all
 from rover import rover
 
-xray_recorder.configure(aws_xray_tracing_name="Rover")
-patch_all()
-
 
 def lambda_handler(event, context):
+    if context.get("aws_request_id") is not None:
+        patch_all()
     response = rover(event["body"].get("username"))
     print(response.json())
     return {
