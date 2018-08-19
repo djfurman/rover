501c31cc93686959d312ae6b40204399ba365cac
Daniel Furman <djfurman@gmail.com>
2018-08-19 16:40:24 -0400

    Format return response correctly

--- lambda_function.py
+++ lambda_function.py
@@ -4,4 +4,8 @@ from rover import rover
 def lambda_handler(event, context):
     response = rover(event["body"].get("username"))
     print(response.json())
-    return response
+    return {
+        "body": response.json(),
+        "headers": dict(response.headers),
+        "status_code": int(response.status_code),
+    }
