9e0773188cf540a4a045ec02776c91f4d08b977a
Daniel Furman <djfurman@gmail.com>
2018-08-19 16:57:34 -0400

    Add x-ray detail tracing - trying to see if we can avoid installing the sdk

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,9 @@
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all
 from rover import rover
+
+xray_recorder.configure(aws_xray_tracing_name="Rover")
+patch_all()
 
 
 def lambda_handler(event, context):
